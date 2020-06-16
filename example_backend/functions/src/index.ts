b6f30e387a32b2e4f2a56d133d046494e6aa83c5
francesco <francesco@camberi.com>
2020-06-16 16:52:06 +0200

    Fixed example database daily cleanup

--- example_backend/functions/src/index.ts
+++ example_backend/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import { DocumentSnapshot } from "firebase-functions/lib/providers/firestore";
 import { deleteInAlgolia, indexInAlgolia } from "./algolia";
+import { eraseDatabase, importDatabaseBackup } from "./util";
 
 
 function updateIndex(snap: functions.Change<DocumentSnapshot>, indexName: string) {
@@ -35,37 +36,15 @@ export const onUsersUpdateIndexAlgolia = functions
         return updateIndex(snap, indexName);
     });
 
+
 /**
  * This function is only used to reset the database daily
  */
 export const scheduledFirestoreImport = functions
     .region("europe-west1")
     .pubsub
-    .schedule('every 24 hours')
+    .schedule("every 24 hours")
     .onRun((context) => {
-
-        const bucket = 'gs://firecms_firestore_backups/2020-06-09T21:01:18_12964'
-        const firestore = require('@google-cloud/firestore');
-        const client = new firestore.v1.FirestoreAdminClient();
-
-        const databaseName = client.databasePath(
-            process.env.GCLOUD_PROJECT,
-            '(default)'
-        );
-
-        return client
-            .importDocuments({
-                name: databaseName,
-                inputUriPrefix: bucket,
-                collectionIds: [],
-            })
-            .then((responses: any) => {
-                const response = responses[0];
-                console.log(`Operation Name: ${response['name']}`);
-                return response;
-            })
-            .catch((err: any) => {
-                console.error(err);
-            });
+        eraseDatabase().then(() => importDatabaseBackup());
     });
 
---
+++ example_backend/functions/src/util.ts
@@ -0,0 +1,54 @@
+import * as functions from "firebase-functions";
+
+const bucket = "gs://firecms_firestore_backups/2020-06-09T21:01:18_12964";
+
+export function eraseDatabase() {
+    const firebase_tools = require("firebase-tools");
+    console.log("Deleting database");
+    const deleteConfig = {
+        project: process.env.GCLOUD_PROJECT,
+        recursive: true,
+        yes: true,
+        token: functions.config().fb.token
+    };
+    return firebase_tools.firestore
+        .delete("/users", deleteConfig)
+        .then(() => firebase_tools.firestore
+            .delete("/products", deleteConfig))
+        .then(() => firebase_tools.firestore
+            .delete("/blog", deleteConfig))
+        .then(() => {
+            console.log("Database erased");
+            return Promise.resolve();
+        })
+        .catch((err: any) => {
+            console.error("error erasing db", err);
+        });
+}
+
+export function importDatabaseBackup() {
+    const firestore = require("@google-cloud/firestore");
+    const client = new firestore.v1.FirestoreAdminClient();
+
+    console.log("Restoring backup database");
+    const databaseName = client.databasePath(
+        process.env.GCLOUD_PROJECT,
+        "(default)"
+    );
+
+    return client
+        .importDocuments({
+            name: databaseName,
+            inputUriPrefix: bucket,
+            collectionIds: []
+        })
+        .then((responses: any) => {
+            const response = responses[0];
+            console.log(`Operation Name: ${response["name"]}`);
+            return response;
+        })
+        .catch((err: any) => {
+            console.error(err);
+        });
+}
+
