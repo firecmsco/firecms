660bb8df23f917f0c93992c3945e2e8034c359b6
francesco <francesco@camberi.com>
2020-10-25 11:33:52 +0100

    Added missing files

---
+++ src/components/EmptyValue.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function EmptyValue() {
+
+    return (
+        <div style={{
+            borderRadius: "9999px",
+            backgroundColor: "rgba(0,0,0,0.05)",
+            width: "18px",
+            height: "6px",
+            display: "inline-block"
+        }}>
+        </div>
+    );
+};
