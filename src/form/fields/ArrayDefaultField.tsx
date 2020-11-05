29f8f783a3c87dca0c60d714200ed60bd516712c
francesco <francesco@camberi.com>
2020-11-05 23:37:12 +0100

    Added reorder feature for default arrays with drag n drop, missing files

--- src/form/fields/ArrayDefaultField.tsx
+++ src/form/fields/ArrayDefaultField.tsx
@@ -1,19 +1,43 @@
-import { Property } from "../../models";
-import { FieldArray, getIn } from "formik";
+import { EntitySchema, Property } from "../../models";
+import { FieldArray, FieldInputProps, getIn } from "formik";
 import {
     Box,
     Button,
+    createStyles,
     FormControl,
     FormHelperText,
     IconButton,
+    makeStyles,
     Paper
 } from "@material-ui/core";
-import { Add, Remove } from "@material-ui/icons";
+import ClearIcon from "@material-ui/icons/Clear";
+import DragHandleIcon from "@material-ui/icons/DragHandle";
 import { formStyles } from "../../styles";
 import { CMSFieldProps } from "../form_props";
-import React from "react";
+import React, { useState } from "react";
 import { FieldDescription } from "../../components";
 import { LabelWithIcon } from "../../components/LabelWithIcon";
+
+import { DropTargetMonitor, useDrag, useDrop } from "react-dnd";
+import { XYCoord } from "dnd-core";
+import { FormFieldProps } from "../index";
+
+
+const useStyles = makeStyles(theme => createStyles({
+    arrayEntry: {
+        marginBottom: theme.spacing(1),
+        opacity: 1
+    },
+    arrayEntryDragging: {
+        marginBottom: theme.spacing(1),
+        border: "1px dashed gray",
+        cursor: "move",
+        opacity: 0.5
+    },
+    handle: {
+        cursor: "move"
+    }
+}));
 
 type ArrayDefaultFieldProps<T> = CMSFieldProps<T[]>;
 
@@ -27,19 +51,50 @@ export default function ArrayDefaultField<T>({
                                                  entitySchema
                                              }: ArrayDefaultFieldProps<T>) {
 
-    const classes = formStyles();
-
-    const ofProperty: Property = property.of as Property;
-
-    const hasValue = field.value && field.value.length > 0;
 
     const fieldError = getIn(errors, field.name);
-    const showError = getIn(touched, field.name) && !!fieldError;
+    const showError = getIn(touched, field.name) && fieldError && !!fieldError.filter((e: any) => !!e).length;
+
+    const ofProperty: Property = property.of as Property;
+    const classes = formStyles();
+    const hasValue = field.value && field.value.length > 0;
+
+    const [internalValue, setInternalValue] = useState<number[]>(
+        field.value ?
+            (field.value as T[]).map((v, index) => getRandomId()) : []);
+
+    function getRandomId() {
+        return Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER));
+    }
 
     return <FieldArray
         name={field.name}
-        render={arrayHelpers =>
-            (
+        render={arrayHelpers => {
+
+            const moveCard = (dragIndex: number, hoverIndex: number) => {
+                const newValue = [...internalValue];
+                const temp = newValue[dragIndex];
+                newValue[dragIndex] = newValue[hoverIndex];
+                newValue[hoverIndex] = temp;
+                setInternalValue(newValue);
+                arrayHelpers.move(dragIndex, hoverIndex);
+            };
+
+            const insertInEnd = () => {
+                const newValue: number[] =
+                    [...internalValue, getRandomId()];
+                setInternalValue(newValue);
+                arrayHelpers.push(null);
+            };
+
+            const remove = (index: number) => {
+                const newValue = [...internalValue];
+                newValue.splice(index, 1);
+                setInternalValue(newValue);
+                arrayHelpers.remove(index);
+            };
+
+            return (
 
                 <FormControl fullWidth error={showError}>
 
@@ -48,56 +103,183 @@ export default function ArrayDefaultField<T>({
                         <LabelWithIcon scaledIcon={true} property={property}/>
                     </FormHelperText>
 
-                    <Paper variant={"outlined"} className={classes.paper}>
-                        {hasValue ? (
-                            field.value.map((entryValue: any, index: number) => {
-                                return (
-                                    <Box key={`field_${index}`}
-                                         mb={1}
-                                         display="flex">
-                                        <Box flexGrow={1}
-                                             width={"100%"}
-                                             key={`field_${field.name}_entryValue`}>
-                                            {createFormField(`${field.name}[${index}]`, ofProperty, includeDescription, underlyingValueHasChanged, entitySchema)}
-                                        </Box>
-                                        <Box width={"50px"}
-                                             display="flex"
-                                             flexDirection="column"
-                                             justifyContent="center"
-                                             alignItems="center">
-                                            <IconButton
-                                                size="small"
-                                                aria-label="insert"
-                                                onClick={() => arrayHelpers.insert(index + 1, undefined)}>
-                                                <Add/>
-                                            </IconButton>
-                                            <IconButton
-                                                size="small"
-                                                aria-label="remove"
-                                                onClick={() => arrayHelpers.remove(index)}>
-                                                <Remove/>
-                                            </IconButton>
-                                        </Box>
-                                    </Box>
-                                );
-                            })
-                        ) : (
-                            <Box p={2}
-                                 justifyContent="center"
-                                 textAlign={"right"}>
-                                <Button variant="outlined"
-                                        color="primary"
-                                        onClick={() => arrayHelpers.push(null)}>
-                                    Add
-                                </Button>
-                            </Box>
-                        )}
+                    <Paper variant={"outlined"}
+                           className={classes.paper}>
+
+                        {hasValue && internalValue.map((entry: number, index: number) => (
+                            <ArrayEntry
+                                key={`array_field_${field.name}_${entry}`}
+                                id={entry}
+                                type={"card_" + field.name}
+                                moveCard={moveCard}
+                                index={index}
+                                field={field}
+                                createFormField={createFormField}
+                                includeDescription={includeDescription}
+                                underlyingValueHasChanged={underlyingValueHasChanged}
+                                entitySchema={entitySchema}
+                                ofProperty={ofProperty}
+                                remove={remove}
+                            />))}
+
+                        <Box p={1}
+                             justifyContent="center"
+                             textAlign={"left"}>
+                            <Button variant="outlined"
+                                    color="primary"
+                                    onClick={insertInEnd}>
+                                Add
+                            </Button>
+                        </Box>
+
                     </Paper>
 
                     {includeDescription &&
                     <FieldDescription property={property}/>}
 
                 </FormControl>
-            )}
+            );
+        }}
     />;
 }
+
+function ArrayEntry<T>(props: {
+    id: any
+    moveCard: (dragIndex: number, hoverIndex: number) => void,
+    type: string
+    index: number,
+    field: FieldInputProps<T>,
+    ofProperty: Property,
+    includeDescription: boolean,
+    createFormField: ({
+                          name,
+                          property,
+                          includeDescription,
+                          underlyingValueHasChanged,
+                          entitySchema
+                      }: FormFieldProps) => React.ReactNode,
+    underlyingValueHasChanged: boolean;
+    entitySchema: EntitySchema,
+    remove: (index: number) => void,
+}) {
+    const {
+        id,
+        moveCard,
+        type,
+        index,
+        field,
+        createFormField,
+        includeDescription,
+        underlyingValueHasChanged,
+        entitySchema,
+        ofProperty,
+        remove
+    } = props;
+
+    const classes = useStyles();
+    const ref = React.useRef<HTMLDivElement>(null);
+    // const refContainer = React.useRef<HTMLDivElement>(null);
+
+    const [, drop] = useDrop({
+        accept: type,
+        hover(item: {
+                  id: number
+                  index: number,
+                  type: string
+              },
+              monitor: DropTargetMonitor) {
+            if (!ref.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+
+            // Don't replace items with themselves
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+
+            if (!ref.current) {
+                return;
+            }
+
+            // Determine rectangle on screen
+            const hoverBoundingRect = ref.current.getBoundingClientRect();
+
+            // Get vertical middle
+            const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+            // Determine mouse position
+            const clientOffset = monitor.getClientOffset();
+
+            // Get pixels to the top
+            const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+
+            // Dragging downwards
+            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY - 50) {
+                return;
+            }
+
+            // Dragging upwards
+            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY + 50) {
+                return;
+            }
+
+            // Time to actually perform the action
+            moveCard(dragIndex, hoverIndex);
+
+            // Note: we're mutating the monitor item here!
+            // Generally it's better to avoid mutations,
+            // but it's good here for the sake of performance
+            // to avoid expensive index searches.
+            item.index = hoverIndex;
+        }
+    });
+
+    const [{ isDragging }, drag, preview] = useDrag({
+        item: { type: type, id, index },
+        collect: (monitor: any) => ({
+            isDragging: monitor.isDragging()
+        })
+    });
+
+    drag(drop(ref));
+
+    return (
+        <div ref={ref}
+             className={isDragging ? classes.arrayEntryDragging : classes.arrayEntry}
+        >
+            <Box key={`field_${index}`}
+                 display="flex">
+                <Box flexGrow={1}
+                     width={"100%"}
+                     key={`field_${field.name}_entryValue`}>
+                    {createFormField(
+                        {
+                            name: `${field.name}[${index}]`,
+                            property: ofProperty,
+                            includeDescription,
+                            underlyingValueHasChanged,
+                            entitySchema,
+                            partOfArray: true
+                        })}
+                </Box>
+                <Box width={"48px"}
+                     display="flex"
+                     flexDirection="column"
+                     alignItems="center">
+                    <div className={classes.handle}>
+                        <DragHandleIcon fontSize={"small"}/>
+                    </div>
+                    <IconButton
+                        size="small"
+                        aria-label="remove"
+                        onClick={() => remove(index)}>
+                        <ClearIcon fontSize={"small"}/>
+                    </IconButton>
+                </Box>
+            </Box>
+        </div>
+    );
+}
+
--- src/test/test_site_config.ts
+++ src/test/test_site_config.ts
@@ -1,4 +1,4 @@
-import { buildSchema, EnumValues } from "../models";
+import { buildCollection, buildSchema, EnumValues } from "../models";
 import { CMSAppProps } from "../CMSApp";
 
 const status = {
@@ -22,25 +22,35 @@ const locales: EnumValues<string> = {
 };
 
 const productSchema = buildSchema({
-    customId: true,
     name: "Product",
     properties: {
         name: {
+            title: "Name",
             validation: { required: true },
-            dataType: "string",
-            title: "Name"
+            dataType: "string"
         },
         price: {
-            dataType: "number",
             title: "Price",
-            validation: { required: true }
+            validation: {
+                required: true,
+                requiredMessage: "You must set a price between 0 and 1000",
+                min: 0,
+                max: 1000
+            },
+            description: "Price with range validation",
+            dataType: "number"
         },
         status: {
             title: "Status",
             validation: { required: true },
             dataType: "string",
+            description: "Should this product be visible in the website",
+            longDescription: "Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.",
             config: {
-                enumValues: status
+                enumValues: {
+                    private: "Private",
+                    public: "Public"
+                }
             }
         },
         categories: {
@@ -50,61 +60,14 @@ const productSchema = buildSchema({
             of: {
                 dataType: "string",
                 config: {
-                    enumValues: categories
-                }
-            }
-        },
-        tags: {
-            title: "Tags",
-            validation: { required: true },
-            dataType: "array",
-            of: {
-                dataType: "string"
-            }
-        },
-        description: {
-            title: "Description",
-            description: "Not mandatory but it'd be awesome if you filled this up",
-            dataType: "string"
-        },
-        published: {
-            title: "Published",
-            dataType: "boolean"
-        },
-        added_on: {
-            title: "Published",
-            dataType: "timestamp"
-        },
-        publisher: {
-            title: "Publisher",
-            dataType: "map",
-            properties: {
-                morning: {
-                    title: "Name",
-                    dataType: "number"
-                },
-                midday: {
-                    title: "External id",
-                    dataType: "number"
-                },
-                locale: {
-                    title: "Locale",
-                    dataType: "string",
-                    config: {
-                        enumValues: locales
+                    enumValues: {
+                        electronics: "Electronics",
+                        books: "Books",
+                        furniture: "Furniture",
+                        clothing: "Clothing",
+                        food: "Food"
                     }
                 }
-            },
-            previewProperties: ["morning", "midday", "locale"]
-        },
-        available_locales: {
-            title: "Available locales",
-            description:
-                "This field is set automatically by Cloud Functions and can't be edited here",
-            dataType: "array",
-            disabled: true,
-            of: {
-                dataType: "string"
             }
         },
         image: {
@@ -117,12 +80,52 @@ const productSchema = buildSchema({
                     acceptedFiles: ["image/*"]
                 }
             }
+        },
+        tags: {
+            title: "Tags",
+            description: "Example of generic array",
+            validation: { required: true },
+            dataType: "array",
+            of: {
+                dataType: "string"
+            }
+        },
+        description: {
+            title: "Description",
+            description: "Not mandatory but it'd be awesome if you filled this up",
+            longDescription: "Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.",
+            dataType: "string",
+            columnWidth: 300
+        },
+        published: {
+            title: "Published",
+            dataType: "boolean",
+            columnWidth: 100
+        },
+        expires_on: {
+            title: "Expires on",
+            dataType: "timestamp"
+        },
+        publisher: {
+            title: "Publisher",
+            description: "This is an example of a map property",
+            dataType: "map",
+            properties: {
+                name: {
+                    title: "Name",
+                    dataType: "string"
+                },
+                external_id: {
+                    title: "External id",
+                    dataType: "string"
+                }
+            }
         }
     }
 });
 
 const subcollections = [
-    {
+    buildCollection({
         name: "Locales",
         relativePath: "locales",
         schema: {
@@ -132,20 +135,17 @@ const subcollections = [
                 title: {
                     title: "Title",
                     validation: { required: true },
-                    includeInListView: true,
                     dataType: "string"
                 },
                 selectable: {
                     title: "Selectable",
                     description: "Is this locale selectable",
-                    includeInListView: true,
                     dataType: "boolean"
                 },
                 video: {
                     title: "Video",
                     dataType: "string",
                     validation: { required: false },
-                    includeInListView: true,
                     storageMeta: {
                         mediaType: "video",
                         storagePath: "videos",
@@ -154,7 +154,7 @@ const subcollections = [
                 }
             }
         }
-    }
+    })
 ];
 
 export const siteConfig: CMSAppProps = {
