e06b933d8c6fa310417e381a4a2cfe5a3f75e66a
francesco <francesco@camberi.com>
2021-07-19 14:15:11 +0200

    Revert example switch to simple app instead of complete one

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import SampleApp from "./SampleApp/SampleApp";
-import { App } from "./SimpleApp";
 
 ReactDOM.render(
-    <App/>,
+    <SampleApp/>,
     document.getElementById("root")
 );
