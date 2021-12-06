bd2363f0b1bfbedc107df7e912c75929a30df73f
francesco <francesco@camberi.com>
2021-12-06 11:43:19 +0100

    Docs

--- src/models/overrides.tsx
+++ src/models/overrides.tsx
@@ -2,13 +2,26 @@ import { AnyProperty } from "./properties";
 import { EntitySchema } from "./entities";
 import { EntityCollection } from "./collections";
 
-export type PartialProperties<M> = Record<any, Partial<AnyProperty>>;
+/**
+ * Use to resolve the schema properties for specific path, entity id or values.
+ * @category Models
+ */
+export type PartialProperties<M> = Record<keyof M, Partial<AnyProperty>>;
 
+/**
+ * Use to resolve the schema properties for specific path, entity id or values.
+ * @category Models
+ */
 export type PartialSchema<M> = Omit<Partial<EntitySchema<M>>, "properties"> &
     {
         properties: PartialProperties<M>
     };
 
+
+/**
+ * Use to resolve the schema properties for specific path, entity id or values.
+ * @category Models
+ */
 export type PartialEntityCollection<M> =
     Omit<Partial<EntityCollection<M>>, "schema"> & {
     schema?: PartialSchema<M>
