170daf9198dbf153b9ea0f51c501dde0f9be759c
francesco <francesco@camberi.com>
2022-04-11 12:44:22 +0200

    Vite update and fix for vite environment variables

--- src/firebase_app/hooks/useFirestoreDataSource.ts
+++ src/firebase_app/hooks/useFirestoreDataSource.ts
@@ -21,7 +21,6 @@ import {
 import {
     computeSchema,
     sanitizeData,
-    traverseValues,
     updateAutoValues
 } from "../../core/utils";
 import {
@@ -167,7 +166,7 @@ export function useFirestoreDataSource({
         if (!textSearchController)
             throw Error("Trying to make text search without specifying a FirestoreTextSearchController");
         const ids = await textSearchController({ path, searchString });
-        if (!ids)
+        if (ids === undefined)
             throw Error("The current path is not supported by the specified FirestoreTextSearchController");
         const promises: Promise<Entity<M> | null>[] = ids
             .map(async (entityId) => {
