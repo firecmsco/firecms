e3a18cdc1c1ff961cb6cd1acdfb2a1d80513053b
Joshua Scott <git@joshuascott.io>
2022-04-13 16:32:57 +0300

    Enable authentication via phone number

--- src/firebase_app/models/auth.tsx
+++ src/firebase_app/models/auth.tsx
@@ -1,4 +1,4 @@
-import { User as FirebaseUser } from "firebase/auth";
+import { ApplicationVerifier, ConfirmationResult, User as FirebaseUser } from "firebase/auth";
 
 import { AuthDelegate } from "../../models";
 
@@ -9,7 +9,7 @@ import { AuthDelegate } from "../../models";
 export type FirebaseSignInProvider =
 // | 'email'
     | "password"
-    // | 'phone'
+    | "phone"
     | "anonymous"
     | "google.com"
     | "facebook.com"
@@ -36,6 +36,8 @@ export type FirebaseAuthDelegate =
 
     authLoading: boolean;
 
+    confirmationResult: void | ConfirmationResult,
+
     googleLogin: () => void;
 
     anonymousLogin: () => void;
@@ -55,6 +57,8 @@ export type FirebaseAuthDelegate =
     fetchSignInMethodsForEmail: (email: string) => Promise<string[]>;
 
     createUserWithEmailAndPassword: (email: string, password: string) => void;
+
+    phoneLogin: (phone: string, applicationVerifier: ApplicationVerifier) => void;
 
     /**
      * Has the user skipped the login process
