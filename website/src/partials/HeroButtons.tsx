b804f1d292c21070b343037c2fccf85787edb506
francesco <francesco@camberi.com>
2022-08-11 22:08:51 +0200

    Website responsive updates

--- website/src/partials/HeroButtons.tsx
+++ website/src/partials/HeroButtons.tsx
@@ -12,7 +12,7 @@ function HeroButtons() {
             Get started
         </a>
         <a
-            className={"btn px-12 py-4 text-white font-bold uppercase bg-blue-600 hover:text-white hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0 sm:ml-4"}
+            className={"btn px-12 py-4 text-white font-bold uppercase bg-blue-600 hover:text-white hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0 sm:ml-4 ml-2"}
             href="https://demo.firecms.co"
             rel="noopener noreferrer"
             target="_blank"
