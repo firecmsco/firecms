3b12eacb59016b07068736121b17230cfffe3c10
francesco <francesco@camberi.com>
2023-01-03 01:10:55 +0100

    Minor

--- example-client-project/vite.config.ts
+++ example-client-project/vite.config.ts
@@ -4,24 +4,24 @@ import path from "path";
 
 // these are the dependencies of the SaaS project
 // Is there a better way to do this?
-// const isExternal = (id: string) => {
-//     return [
-//         "@camberi/firecms",
-//         "@camberi/firecms_collection_editor",
-//         "@emotion/react",
-//         "@emotion/styled",
-//         "@mui/icons-material",
-//         "@mui/lab",
-//         "@mui/material",
-//         "algoliasearch",
-//         "firebase",
-//         "react",
-//         "react-dom",
-//         "react-router",
-//         "react-router-dom"
-//     ].includes(id);
-// };
-const isExternal = (id: string) => !id.startsWith(".") && !path.isAbsolute(id);
+const isExternal = (id: string) => {
+    return [
+        "@camberi/firecms",
+        // "@camberi/firecms_collection_editor",
+        // "@emotion/react",
+        // "@emotion/styled",
+        // "@mui/icons-material",
+        // "@mui/lab",
+        // "@mui/material",
+        // "algoliasearch",
+        // "firebase",
+        // "react",
+        // "react-dom",
+        // "react-router",
+        // "react-router-dom"
+    ].includes(id);
+};
+// const isExternal = (id: string) => !id.startsWith(".") && !path.isAbsolute(id);
 
 
 // https://vitejs.dev/config/
