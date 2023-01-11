9404262b4f411f4feb4ffaeea1b17228973f6f63
francesco <francesco@camberi.com>
2023-01-11 18:25:21 +0100

    Removed babel config

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -23,9 +23,6 @@ export default defineConfig(() => ({
         }
     },
     plugins: [react({
-        jsxImportSource: "@emotion/react",
-        babel: {
-            plugins: ["@emotion/babel-plugin"]
-        }
+        jsxImportSource: "@emotion/react"
     })]
 }));
