bee261162a2f91736415935df523d69dcd313da5
francesco <francesco@camberi.com>
2023-06-12 17:52:57 +0200

    Clean up

--- example-client-project/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-console.log("main");
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<div >yo</div>
-	</React.StrictMode>
-);
