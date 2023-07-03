d07a353c2b8c324fadae3b8a4898506b0a4512c6
francesco <francesco@camberi.com>
2023-07-03 21:48:35 +0200

    Disabling in example source code the local plugin

--- example/src/SampleApp/SampleApp.tsx
+++ example/src/SampleApp/SampleApp.tsx
@@ -8,7 +8,7 @@ import { getAnalytics, logEvent } from "firebase/analytics";
 import { User as FirebaseUser } from "firebase/auth";
 
 import { AppCheckOptions, Authenticator, CMSView, FirebaseCMSApp } from "firecms";
-import { useDataEnhancementPlugin } from "@firecms/data_enhancement";
+// import { useDataEnhancementPlugin } from "@firecms/data_enhancement";
 
 import { IconButton, Tooltip } from "@mui/material";
 import { GitHub } from "@mui/icons-material";
@@ -110,22 +110,22 @@ function SampleApp() {
         logEvent(analytics, event, data);
     }, []);
 
-    const dataEnhancementPlugin = useDataEnhancementPlugin({
-        getConfigForPath: ({ path }) => {
-            if (process.env.NODE_ENV !== "production")
-                return true;
-            if (path === "books")
-                return true;
-            return false;
-        }
-    });
+    // const dataEnhancementPlugin = useDataEnhancementPlugin({
+    //     getConfigForPath: ({ path }) => {
+    //         if (process.env.NODE_ENV !== "production")
+    //             return true;
+    //         if (path === "books")
+    //             return true;
+    //         return false;
+    //     }
+    // });
 
     return <FirebaseCMSApp
         name={"My Online Shop"}
         // appCheckOptions={appCheckOptions}
         authentication={myAuthenticator}
         allowSkipLogin={true}
-        plugins={[dataEnhancementPlugin]}
+        // plugins={[dataEnhancementPlugin]}
         signInOptions={[
             "password",
             "google.com"
