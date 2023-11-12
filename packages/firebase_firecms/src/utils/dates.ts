7ea76262a70d4830fa5b2d600c2687b1b2484eed
francesco <francesco@camberi.com>
2023-11-12 11:50:52 +0100

    Fix for setting dates to midnight

--- packages/firebase_firecms/src/utils/dates.ts
+++ packages/firebase_firecms/src/utils/dates.ts
@@ -1,6 +1,7 @@
 import { Timestamp } from "firebase/firestore";
 
 export function setDateToMidnight(input?: Timestamp): Timestamp | undefined {
+    if (!(input instanceof Timestamp)) return input;
     if (!input) return input;
     const date = input.toDate();
     date.setHours(0, 0, 0, 0);
