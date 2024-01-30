4ca09a6c0c8c7bfff13fb85c466adce7ca75c181
francesco <francesco@camberi.com>
2024-01-30 16:51:11 +0100

    Minor

--- packages/firecms/src/components/subscriptions/ProjectSubscriptionPlans.tsx
+++ packages/firecms/src/components/subscriptions/ProjectSubscriptionPlans.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { CircularProgressCenter, useBrowserTitleAndIcon } from "@firecms/core";
-import { AutoAwesomeIcon, Card, Chip, cn, Typography, } from "@firecms/ui";
+import { useBrowserTitleAndIcon } from "@firecms/core";
+import { AutoAwesomeIcon, Card, Chip, CircularProgress, cn, Typography, } from "@firecms/ui";
 import { useSubscriptionsForUserController } from "../../hooks/useSubscriptionsForUserController";
 import { ProductView } from "./ProductView";
 import { getPriceString, getStatusText } from "../settings/common";
@@ -14,7 +14,10 @@ export function ProjectSubscriptionPlans({ uid }: {
 }) {
 
     const { backendFirebaseApp } = useFireCMSBackend();
-    const { subscriptionPlan, projectId } = useProjectConfig();
+    const {
+        subscriptionPlan,
+        projectId
+    } = useProjectConfig();
     if (!subscriptionPlan)
         throw new Error("No subscription plan");
 
@@ -30,71 +33,82 @@ export function ProjectSubscriptionPlans({ uid }: {
 
     const projectSubscriptions = getSubscriptionsForProject(projectId);
 
-    if (projectSubscriptions === undefined || products === undefined) {
-        return <CircularProgressCenter/>
-    }
+    const loading = projectSubscriptions === undefined || products === undefined;
 
-    const cloudProducts = products.filter(p => p.metadata?.type === "cloud_plus" || p.metadata?.type === "cloud_pro");
+    const cloudProducts = (products ?? []).filter(p => p.metadata?.type === "cloud_plus" || p.metadata?.type === "cloud_pro");
 
     const plusProduct = cloudProducts.find(p => p.metadata?.type === "cloud_plus");
     const plusSubscription = projectSubscriptions.find(s => s.product.metadata?.type === "cloud_plus");
 
     return (
-        <div className={"grid grid-cols-12 gap-4 items-center"}>
+        <div className={"relative"}>
 
-            <div className={"col-span-12 md:col-span-7 flex flex-col gap-2"}>
+            {loading &&
+                <div className={"absolute w-full h-full flex items-center justify-center"}><CircularProgress/></div>}
 
-                <Typography variant={"h4"} className="mt-4 mb-2">Subscription Plan</Typography>
+            <div className={cn("grid grid-cols-12 gap-4 items-center", loading ? "collapse" : "")}>
 
-                <Typography
-                    variant={"subtitle1"}
-                    className="my-2">
-                    This project is on the <PlanChip subscriptionPlan={subscriptionPlan}/>
+                <div className={"col-span-12 md:col-span-7 flex flex-col gap-2"}>
 
-                </Typography>
+                    <Typography variant={"h4"} className="mt-4 mb-2">Subscription Plan</Typography>
 
-                {subscriptionPlan !== "cloud_plus" && plusProduct && <ProductView
-                    product={plusProduct}
-                    projectId={projectId}
-                    subscribe={subscribe}/>}
+                    <Typography
+                        variant={"subtitle1"}
+                        className="my-2">
+                        This project is on the <PlanChip subscriptionPlan={subscriptionPlan}/>
 
-                {subscriptionPlan === "cloud_plus" && plusSubscription &&
-                    <CurrentSubscriptionView subscription={plusSubscription}/>}
+                    </Typography>
 
-                <StripeDisclaimer/>
+                    {subscriptionPlan !== "cloud_plus" && plusProduct && <ProductView
+                        product={plusProduct}
+                        projectId={projectId}
+                        subscribe={subscribe}/>}
 
-            </div>
+                    {subscriptionPlan === "cloud_plus" && plusSubscription &&
+                        <CurrentSubscriptionView subscription={plusSubscription}/>}
 
-            <div className={cn("col-span-12 md:col-span-5")}>
-                <Card
-                    className={"p-6 bg-amber-200 dark:bg-amber-700 border-amber-300 dark:border-amber-800 flex flex-col gap-4"}>
+                    <StripeDisclaimer/>
 
-                    {subscriptionPlan === "free" && <div>Upgrade to the <b>PLUS</b> plan to enable the following
-                        features:</div>}
+                </div>
 
-                    {subscriptionPlan === "cloud_plus" && <div>By being on the <b>PLUS</b> plan you are enjoying the
-                        following features:</div>}
+                <div className={cn("col-span-12 md:col-span-5")}>
+                    <Card
+                        className={"p-6 bg-amber-200 dark:bg-amber-700 border-amber-300 dark:border-amber-800 flex flex-col gap-4"}>
 
-                    <ul className={"px-2 text-base"}>
-                        <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>Unlimited
-                            users and roles
-                        </li>
-                        <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>Theme and
-                            logo customization
-                        </li>
-                        <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>Custom
-                            user
-                            roles
-                        </li>
-                        <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>GPT-4
-                            content generation
-                        </li>
-                    </ul>
-                </Card>
+                        {subscriptionPlan === "free" && <div>Upgrade to the <b>PLUS</b> plan to enable the following
+                            features:</div>}
 
+                        {subscriptionPlan === "cloud_plus" && <div>By being on the <b>PLUS</b> plan you are enjoying the
+                            following features:</div>}
+
+                        <ul className={"px-2 text-base"}>
+                            <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>
+                                Local text search
+                            </li>
+                            <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>Unlimited
+                                users and roles
+                            </li>
+                            <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>Theme and
+                                logo customization
+                            </li>
+                            <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>
+                                Custom fields and custom views
+                            </li>
+                            <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>
+                                Custom user roles
+                            </li>
+                            <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>GPT-4
+                                content generation
+                            </li>
+                            <li className={"flex gap-4 items-center py-0.5"}><AutoAwesomeIcon size={"small"}/>
+                                Unlimited data export
+                            </li>
+                        </ul>
+                    </Card>
+
+                </div>
             </div>
         </div>
-
     );
 
 }
@@ -107,7 +121,10 @@ function CurrentSubscriptionView({
                                      subscription,
                                  }: CurrentSubscriptionViewProps) {
 
-    const { getBackendAuthToken, projectsApi } = useFireCMSBackend();
+    const {
+        getBackendAuthToken,
+        projectsApi
+    } = useFireCMSBackend();
 
     const statusText = getStatusText(subscription);
     const [stripePortalUrl, setStripePortalUrl] = useState<string | undefined>(undefined);
