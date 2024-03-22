da56b024abd994f7bab01ab4407458b496d03e21
francesco <francesco@camberi.com>
2024-03-22 22:30:36 +0100

    Minor

--- examples/example_pro/src/FirestoreApp/TestEditorView.tsx
+++ examples/example_pro/src/FirestoreApp/TestEditorView.tsx
@@ -36,7 +36,7 @@ export function TestEditorView() {
                 // }}
                 onMarkdownContentChange={(content) => {
                     // console.log("markdown content")
-                    console.log(content);
+                    // console.log(content);
                     window.localStorage.setItem("editor-content", content);
                 }}
                 handleImageUpload={async (file: File) => {
