ebc711a9f8a034d71e2b1e54bd7f06d94ce448ef
francesco <francesco@camberi.com>
2024-03-25 16:12:16 +0100

    v3.0.0-canary.13

---
+++ packages/firecms_cli/vite.config.ts
@@ -0,0 +1,38 @@
+// @ts-ignore
+import path from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"
+
+const isExternal = (id: string) => !id.startsWith(".") && !path.isAbsolute(id);
+
+export default defineConfig(() => ({
+    esbuild: {
+        logOverride: { "this-is-undefined-in-esm": "silent" }
+    },
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            name: "FireCMS Firebase",
+            fileName: (format) => `index.${format}.js`
+        },
+        target: "esnext",
+        sourcemap: true,
+        rollupOptions: {
+            external: isExternal
+        }
+    },
+    resolve: {
+        alias: {
+            "@firecms/core": path.resolve(__dirname, "../firecms_core/src"),
+            "@firecms/ui": path.resolve(__dirname, "../ui/src"),
+            "@firecms/schema_inference": path.resolve(__dirname, "../schema_inference/src"),
+            "@firecms/collection_editor": path.resolve(__dirname, "../collection_editor/src"),
+            "@firecms/data_enhancement": path.resolve(__dirname, "../data_enhancement/src"),
+            "@firecms/data_import_export": path.resolve(__dirname, "../data_import_export/src"),
+        }
+    },
+    plugins: [
+        react({})
+    ]
+}));
