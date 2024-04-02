52fb76fe7907f173d599562a847a05389cdbc6eb
francesco <francesco@camberi.com>
2024-04-02 14:25:42 +0200

    Removed state from user local persistence

--- packages/firecms_core/src/hooks/useBuildLocalConfigurationPersistence.tsx
+++ packages/firecms_core/src/hooks/useBuildLocalConfigurationPersistence.tsx
@@ -1,10 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { PartialEntityCollection, UserConfigurationPersistence } from "../types";
 import { mergeDeep, stripCollectionPath } from "../util";
 
 export function useBuildLocalConfigurationPersistence(): UserConfigurationPersistence {
 
-    const [configCache, setConfigCache] = useState<Record<string, PartialEntityCollection>>({});
+    const configCache = useRef<Record<string, PartialEntityCollection>>({});
 
     const getCollectionFromStorage = useCallback((storageKey: string) => {
         const item = localStorage.getItem(storageKey);
@@ -13,20 +13,19 @@ export function useBuildLocalConfigurationPersistence(): UserConfigurationPersis
 
     const getCollectionConfig = useCallback(<M extends Record<string, any>>(path: string): PartialEntityCollection<M> => {
         const storageKey = `collection_config::${stripCollectionPath(path)}`;
-        if (configCache[storageKey]) {
-            return configCache[storageKey] as PartialEntityCollection<M>;
+        if (configCache.current[storageKey]) {
+            return configCache.current[storageKey] as PartialEntityCollection<M>;
         }
         return getCollectionFromStorage(storageKey);
-    }, [configCache, getCollectionFromStorage]);
+    }, [getCollectionFromStorage]);
 
     const onCollectionModified = useCallback(<M extends Record<string, any>>(path: string, data: PartialEntityCollection<M>) => {
         const storageKey = `collection_config::${stripCollectionPath(path)}`;
         localStorage.setItem(storageKey, JSON.stringify(data));
-        setConfigCache((currentCache) => {
-            const cachedConfig = currentCache[storageKey];
-            const newConfig = mergeDeep(cachedConfig ?? getCollectionFromStorage(path), data);
-            return (mergeDeep(currentCache, newConfig));
-        });
+        const currentCache = configCache.current;
+        const cachedConfig = currentCache[storageKey];
+        const newConfig = mergeDeep(cachedConfig ?? getCollectionFromStorage(path), data);
+        configCache.current = mergeDeep(currentCache, newConfig);
     }, [getCollectionFromStorage]);
 
     const [recentlyVisitedPaths, _setRecentlyVisitedPaths] = useState<string[]>([]);
