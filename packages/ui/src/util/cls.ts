8fb738b6abf384c0cba679f293b049a0ac56a7c2
francesco <francesco@camberi.com>
2024-06-17 16:29:40 +0200

    v3.0.0-canary.58

--- packages/ui/src/util/cls.ts
+++ packages/ui/src/util/cls.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...classes: ClassValue[]) {
+export function cls(...classes: ClassValue[]) {
     return twMerge(clsx(classes))
 }
