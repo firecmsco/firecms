45a9fa0cf6ac971abe6dac851e58b943fe3b5b12
francesco <francesco@camberi.com>
2024-10-24 02:30:49 +0200

    Removed editor table

--- packages/editor/src/editor.tsx
+++ packages/editor/src/editor.tsx
@@ -7,11 +7,6 @@ import { Color } from "@tiptap/extension-color";
 import BulletList from "@tiptap/extension-bullet-list"
 import Highlight from "@tiptap/extension-highlight";
 
-import Table from "@tiptap/extension-table"
-import TableCell from "@tiptap/extension-table-cell"
-import TableHeader from "@tiptap/extension-table-header"
-import TableRow from "@tiptap/extension-table-row"
-
 import { EditorBubble, type JSONContent } from "./components";
 
 import { NodeSelector } from "./selectors/node-selector";
@@ -159,13 +154,7 @@ export const FireCMSEditor = ({
                 aiController,
                 onDisabledAutocompleteClick
             })
-        }),
-        Table.configure({
-            resizable: false,
-        }),
-        TableRow,
-        TableHeader,
-        TableCell,
+        })
     ]), []);
 
     return (
