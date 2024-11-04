7a8c7647b093912caebee552212393787cc135b3
francesco <francesco@camberi.com>
2024-11-04 20:46:48 +0100

    Added eslint config files in templates

---
+++ packages/cli/templates/template_pro/eslint.config.js
@@ -0,0 +1,39 @@
+export default [
+    {
+        rules: {
+            "no-undef": "off",
+            "space-before-function-paren": 0,
+            "react/prop-types": 0,
+            "react/jsx-handler-names": 0,
+            "react/jsx-fragments": 0,
+            "react/no-unused-prop-types": 0,
+            "react/react-in-jsx-scope": "off",
+            "import/export": 0,
+            "no-use-before-define": "off",
+            "no-empty-pattern": "off",
+            "no-unused-vars": "warn",
+            "no-shadow": "warn",
+            "padded-blocks": "off",
+            "brace-style": "off",
+            "curly": "off",
+            "semi": 0,
+            "key-spacing": "warn",
+            "no-trailing-spaces": "warn",
+            "comma-dangle": "warn",
+            "no-multi-spaces": "warn",
+            "comma-spacing": "warn",
+            "keyword-spacing": "warn",
+            "no-multiple-empty-lines": "warn",
+            "object-curly-spacing": ["warn", "always"],
+            "multiline-ternary": "off",
+            "space-before-blocks": "warn",
+            "object-property-newline": "warn",
+            "eol-last": "warn",
+            "spaced-comment": "off",
+            "indent": [0, 4],
+            "quotes": [1, "double", {
+                avoidEscape: true,
+            }]
+        }
+    }
+];
