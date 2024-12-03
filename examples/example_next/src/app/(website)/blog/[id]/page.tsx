de7879fab90279ab46ad56a59ab66a575609f31c
francesco <francesco@camberi.com>
2024-12-03 02:08:04 +0100

    Minor

--- examples/example_next/src/app/(website)/blog/[id]/page.tsx
+++ examples/example_next/src/app/(website)/blog/[id]/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { getBlogEntry } from "@/app/common/database";
 import { BlogEntryView } from "@/app/common/components/BlogEntryView";
-import { CenteredView } from "@firecms/ui";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
 
     const data = await getBlogEntry(id);
     if (!data) {
-        return <CenteredView>Blog entry not found</CenteredView>;
+        return <div>Blog entry not found</div>;
     }
     return (
         <BlogEntryView blogEntry={data}/>
