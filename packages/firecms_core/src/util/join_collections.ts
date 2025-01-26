d667e27f8401176cbdd54db4f9a3fe4012c7078f
francesco <francesco@camberi.com>
2025-01-26 16:15:01 +0100

    Flagging merged collections as merged

--- packages/firecms_core/src/util/join_collections.ts
+++ packages/firecms_core/src/util/join_collections.ts
@@ -115,6 +115,9 @@ export function mergeCollection(target: EntityCollection,
         if (modifiedCollection)
             resultCollection = modifiedCollection;
     }
+
+    // @ts-ignore
+    resultCollection["merged"] = true;
     return resultCollection
 }
 
