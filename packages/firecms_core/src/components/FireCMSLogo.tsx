b596b25e71c2ac114e01390fa34e1ea3c3475b55
francesco <francesco@camberi.com>
2025-06-19 17:31:02 +0200

    Updated logo

--- packages/firecms_core/src/components/FireCMSLogo.tsx
+++ packages/firecms_core/src/components/FireCMSLogo.tsx
@@ -14,59 +14,15 @@ export function FireCMSLogo({
     return (
         <svg
             width={width ?? "100%"} height={height ?? "100%"}
-            viewBox="0 0 599 599" version="1.1"
+            version="1.1"
             style={style}
             className={className}
-            xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                <radialGradient cx="28.6213569%" cy="43.1133328%"
-                                fx="28.6213569%"
-                                fy="43.1133328%" r="71.5003456%"
-                                gradientTransform="translate(0.286214,0.431133),rotate(3.343450),scale(1.000000,0.996175),translate(-0.286214,-0.431133)"
-                                id="radialGradient-1">
-                    <stop stopColor="#FF5B79" offset="0%"/>
-                    <stop stopColor="#FA5574" offset="28.0930803%"/>
-                    <stop stopColor="#EC4C51" offset="44.7242531%"/>
-                    <stop stopColor="#9543C1" offset="71.4578165%"/>
-                    <stop stopColor="#3857B3" offset="100%"/>
-                </radialGradient>
-                <radialGradient cx="53.6205516%" cy="47.2473036%"
-                                fx="53.6205516%"
-                                fy="47.2473036%" r="50.8229649%"
-                                gradientTransform="translate(0.536206,0.472473),rotate(90.000000),scale(1.000000,1.206631),translate(-0.536206,-0.472473)"
-                                id="radialGradient-2">
-                    <stop stopColor="#68294F" stopOpacity="0" offset="0%"/>
-                    <stop stopColor="#5E2548" stopOpacity="0.04641108"
-                          offset="75.3503173%"/>
-                    <stop stopColor="#0D060B" stopOpacity="0.437431709"
-                          offset="100%"/>
-                </radialGradient>
-                <radialGradient cx="53.8605015%" cy="48.1990423%"
-                                fx="53.8605015%"
-                                fy="48.1990423%" r="59.9151549%"
-                                gradientTransform="translate(0.538605,0.481990),rotate(180.000000),scale(1.000000,0.925027),translate(-0.538605,-0.481990)"
-                                id="radialGradient-3">
-                    <stop stopColor="#68294F" stopOpacity="0" offset="0%"/>
-                    <stop stopColor="#5E2548" stopOpacity="0.04641108"
-                          offset="84.0867343%"/>
-                    <stop stopColor="#FF0000" stopOpacity="0.567324765"
-                          offset="100%"/>
-                </radialGradient>
-            </defs>
-            <g id="Page-1" stroke="none" strokeWidth="1" fill="none"
-               fillRule="evenodd">
-                <g id="firecms_logo">
-                    <circle fill="url(#radialGradient-1)" cx="299.5"
-                            cy="299.5"
-                            r="299.5"/>
-                    <circle fill="url(#radialGradient-2)" cx="299.5"
-                            cy="299.5"
-                            r="299.5"/>
-                    <circle fill="url(#radialGradient-3)" cx="299.5"
-                            cy="299.5"
-                            r="299.5"/>
-                </g>
-            </g>
+            viewBox="0 0 583 583" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="291.5" cy="291.5" r="291.5" fill="#0070F4"/>
+            <ellipse cx="292" cy="291.5" rx="173" ry="173.5" fill="#FF3773"/>
+            <path
+                d="M465 291.5C465 268.847 460.525 246.416 451.831 225.487C443.137 204.558 430.394 185.542 414.329 169.524C398.265 153.506 379.194 140.8 358.204 132.131C337.215 123.462 314.719 119 292 119C269.281 119 246.785 123.462 225.796 132.131C204.806 140.8 185.735 153.506 169.671 169.524C153.606 185.542 140.863 204.558 132.169 225.487C123.475 246.416 119 268.847 119 291.5L292 291.5H465Z"
+                fill="#FFA400"/>
         </svg>
     );
 
