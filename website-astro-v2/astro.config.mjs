48b3eab27eb839d3b4310bff6b79d09742ba7567
francesco <francesco@camberi.com>
2025-10-20 15:48:56 +0200

    Astro v2 updates

--- website-astro-v2/astro.config.mjs
+++ website-astro-v2/astro.config.mjs
@@ -1,27 +1,30 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dataki.ai',
-    vite: {
-        plugins: [tailwindcss()],
-        esbuild: {
-            jsx: "automatic",
-            jsxImportSource: "react"
-        }
-    },
-
+    site: "https://firecms.co",
     integrations: [
         react({
             experimentalReactChildren: true
         }),
         sitemap()
-    ]
+    ],
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@firecms/ui": path.resolve(new URL(".", import.meta.url).pathname, "../packages/ui")
+            }
+        },
+        server: {
+            fs: {
+                allow: [path.resolve(new URL(".", import.meta.url).pathname, ".."), path.resolve(new URL(".", import.meta.url).pathname, ".")]
+            }
+        }
+    },
 });
